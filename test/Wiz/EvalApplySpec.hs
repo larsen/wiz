bb587402b55518996b32b98394eb747107428dc4
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2016-12-29 16:49:25 +0100

    shorter, more readable tests

--- test/Wiz/EvalApplySpec.hs
+++ test/Wiz/EvalApplySpec.hs
@@ -21,6 +21,13 @@ parseForm str = do
     Left err -> error "Something gone wrong!"
     Right f  -> return f
 
+testForm programFile form expectedResults = do
+  prg <- loadProgram programFile
+  env <- W.runProgram emptyEnv $ fromMaybe (Program []) prg
+  expr <- parseForm form
+  results <- W.eval expr env
+  results `shouldBe` (env, expectedResults)
+
 spec = describe "main" $ do
   
   describe "parse" $ do
@@ -53,103 +60,90 @@ spec = describe "main" $ do
 
   describe "eval" $ do
 
-    it "eval numeric expressions as integers" $ do
-      prg <- loadProgram "init.scm"
-      env <- W.runProgram emptyEnv $ fromMaybe (Program []) prg
-      expr <- parseForm "1"
-      results <- W.eval expr env
-      results `shouldBe` (env, Just (E $ Number 1))
+    it "eval numeric expressions as integers" $
+      testForm
+        "init.scm"
+        "1"
+        (Just (E $ Number 1))
 
     it "eval simple arithmetic operations" $ do
-      prg <- loadProgram "init.scm"
-      env <- W.runProgram emptyEnv $ fromMaybe (Program []) prg
-      expr <- parseForm "(+ 2 2)"
-      results <- W.eval expr env
-      results `shouldBe` (env, Just (E $ Number 4))
+      testForm
+        "init.scm"
+        "(+ 2 2)"
+        (Just (E $ Number 4))
+    
 
     it "eval simple comparison operator" $ do
-      prg <- loadProgram "init.scm"
-      env <- W.runProgram emptyEnv $ fromMaybe (Program []) prg
-      expr <- parseForm "(< 1 2)"
-      results <- W.eval expr env
-      results `shouldBe` (env, Just (E $ Boolean True))
+      testForm
+        "init.scm"
+        "(< 1 2)"
+        (Just (E $ Boolean True))
 
     it "eval simple comparison operator /2" $ do
-      prg <- loadProgram "init.scm"
-      env <- W.runProgram emptyEnv $ fromMaybe (Program []) prg
-      expr <- parseForm "(> 1 2)"
-      results <- W.eval expr env
-      results `shouldBe` (env, Just (E $ Boolean False))
+      testForm
+        "init.scm"
+        "(> 1 2)"
+        (Just (E $ Boolean False))
 
     it "eval simple comparison operator /3" $ do
-      prg <- loadProgram "init.scm"
-      env <- W.runProgram emptyEnv $ fromMaybe (Program []) prg
-      expr <- parseForm "(> 3 2 5)"
-      results <- W.eval expr env
-      results `shouldBe` (env, Just (E $ Boolean False))
+      testForm
+        "init.scm"
+        "(> 3 2 5)"
+        (Just (E $ Boolean False))
 
     it "eval simple comparison operator /3" $ do
-      prg <- loadProgram "init.scm"
-      env <- W.runProgram emptyEnv $ fromMaybe (Program []) prg
-      expr <- parseForm "(>= 3 3 5)"
-      results <- W.eval expr env
-      results `shouldBe` (env, Just (E $ Boolean False))
+      testForm
+        "init.scm"
+        "(> 3 3 5)"
+        (Just (E $ Boolean False))
 
     it "eval function calls /square" $ do
-      prg <- loadProgram "test/square.scm"
-      env <- W.runProgram emptyEnv $ fromMaybe (Program []) prg
-      expr <- parseForm "(square 10)"
-      results <- W.eval expr env
-      results `shouldBe` (env, Just (E $ Number 100))
+      testForm
+        "test/square.scm"
+        "(square 10)"
+        (Just (E $ Number 100))
 
     it "eval recursive function calls /fact" $ do
-      prg <- loadProgram "init.scm"
-      env <- W.runProgram emptyEnv $ fromMaybe (Program []) prg
-      expr <- parseForm "(fact 10)"
-      results <- W.eval expr env
-      results `shouldBe` (env, Just (E $ Number 3628800))
+      testForm
+        "init.scm"
+        "(fact 10)"
+        (Just (E $ Number 3628800))
 
     it "eval recursive function calls /map" $ do
-      prg <- loadProgram "init.scm"
-      env <- W.runProgram emptyEnv $ fromMaybe (Program []) prg
-      expr <- parseForm "(map fact '(1 2 3))"
-      results <- W.eval expr env
-      (snd results) `shouldBe` Just (E $ List [Number 1, Number 2, Number 6])
+      testForm
+        "init.scm"
+        "(map fact '(1 2 3))"
+        (Just (E $ List [Number 1, Number 2, Number 6]))
 
     it "eval another recursive function calls /length" $ do
-      prg <- loadProgram "init.scm"
-      env <- W.runProgram emptyEnv $ fromMaybe (Program []) prg
-      expr <- parseForm "(length '(1 2 3))"
-      results <- W.eval expr env
-      results `shouldBe` (env, Just (E $ Number 3))
+      testForm
+        "init.scm"
+        "(length '(1 2 3))"
+        (Just (E $ Number 3))
 
     it "eval (let) form" $ do
-      prg <- loadProgram "init.scm"
-      env <- W.runProgram emptyEnv $ fromMaybe (Program []) prg
-      expr <- parseForm "(let ((a 10) (b 20)) a)"
-      results <- W.eval expr env
-      results `shouldBe` (env, Just (E $ Number 10))
+      testForm
+        "init.scm"
+        "(let ((a 10) (b 20)) a)"
+        (Just (E $ Number 10))
 
     it "eval (let) form /2" $ do
-      prg <- loadProgram "init.scm"
-      env <- W.runProgram emptyEnv $ fromMaybe (Program []) prg
-      expr <- parseForm "(let ((a 10) (b 20)) (+ a b))"
-      results <- W.eval expr env
-      results `shouldBe` (env, Just (E $ Number 30))
+      testForm
+        "init.scm"
+        "(let ((a 10) (b 20)) (+ a b))"
+        (Just (E $ Number 30))
 
     it "eval closure form" $ do
-      prg <- loadProgram "init.scm"
-      env <- W.runProgram emptyEnv $ fromMaybe (Program []) prg
-      expr <- parseForm "(add1 10)"
-      results <- W.eval expr env
-      results `shouldBe` (env, Just (E $ Number 11))
+      testForm
+        "init.scm"
+        "(add1 10)"
+        (Just (E $ Number 11))
 
     it "procedure returning closure" $ do
-      prg <- loadProgram "test/closure.scm"
-      env <- W.runProgram emptyEnv $ fromMaybe (Program []) prg
-      expr <- parseForm "(m 10)"
-      results <- W.eval expr env
-      results `shouldBe` (env, Just (E $ Number 20))
+       testForm
+        "test/closure.scm"
+        "(m 10)"
+        (Just (E $ Number 20))
 
     it "count change example" $ do
       prg <- loadProgram "init.scm"
@@ -162,18 +156,16 @@ spec = describe "main" $ do
 
   describe "set! instructions" $ do
     it "eval set!" $ do
-      prg <- loadProgram "test/set.scm"
-      env <- W.runProgram emptyEnv $ fromMaybe (Program []) prg
-      expr <- parseForm "a"
-      results <- W.eval expr env
-      results `shouldBe` (env, Just (E $ Number 20))
+      testForm
+        "test/set.scm"
+        "a"
+        (Just (E $ Number 20))
 
     it "eval set-car!" $ do
-      prg <- loadProgram "test/set-car.scm"
-      env <- W.runProgram emptyEnv $ fromMaybe (Program []) prg
-      expr <- parseForm "list"
-      results <- W.eval expr env
-      results `shouldBe` (env, Just (E $ List [Number 2, Number 2, Number 3]))
+      testForm
+        "test/set-car.scm"
+        "list"
+        (Just (E $ List [Number 2, Number 2, Number 3]))
 
     it "eval set-cdr!" $ do
       prg <- loadProgram "test/set-cdr.scm"
