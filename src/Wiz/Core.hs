b2d6cd00790935f5b27d87fee5ff7441172a7930
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2017-07-01 16:56:18 +0200

    moves core functions implementation in a separate module

---
+++ src/Wiz/Core.hs
@@ -0,0 +1,62 @@
+module Wiz.Core (
+    equal,
+    car,
+    cdr,
+    cons,
+    list,
+    nil,
+    Wiz.Core.not,
+    Wiz.Core.or,
+    Wiz.Core.and,
+    pair,
+    evalBool
+  ) where
+
+import Wiz.Types
+import Wiz.Environment
+
+equal :: Value -> Value -> Expression
+equal x y | x == y = Boolean True
+          | otherwise = Boolean False
+
+car :: Value -> Expression
+car (E (List (x:_))) = x
+car (E (List [])) = List []
+car _ = error "car applied to non list expression!"
+
+cdr :: Value -> Expression
+cdr (E (List (_:xs))) = List xs
+cdr (E (List [])) = List []
+cdr _ = error "cdr applied to non list expression!"
+
+cons :: Value -> Value -> Expression
+-- cons v1 v2
+--   | trace ("-> cons v1:" ++ show v1 ++ ", v2:" ++ show v2) False = undefined
+cons (E x) (E (List ys)) = List $ x:ys
+cons (E x) (E e) = List [x,e]
+
+list :: [Expression] -> Expression
+list = List
+
+nil :: Value -> Expression
+nil (E (List [])) = Boolean True
+nil _ = Boolean False
+
+not :: Value -> Expression
+not (E (Boolean False)) = Boolean True
+not _ = Boolean False
+
+evalBool :: Value -> Bool
+-- evalBool v | trace ("evalBool " ++ show v) False = undefined
+evalBool (E (Boolean b)) = b
+evalBool e = error $ "evalBool " ++ show e
+
+or :: [Value] -> Expression
+or = Boolean . any evalBool
+
+and :: [Value] -> Expression
+and = Boolean . all evalBool
+
+pair :: Value -> Expression
+pair (E (List (x:_))) = Boolean True
+pair _ = Boolean False
--- src/Wiz/EvalApply.hs
+++ src/Wiz/EvalApply.hs
@@ -4,6 +4,7 @@ module Wiz.EvalApply (
   ) where
 
 import Wiz.Types
+import Wiz.Core
 import Wiz.Environment
 import Wiz.Parser
 import qualified Data.Map as Map
@@ -66,59 +67,13 @@ evalNum :: Value -> Double
 evalNum (E (Number n)) = n
 evalNum e = error $ "evalNum " ++ show e
 
-evalBool :: Value -> Bool
--- evalBool v | trace ("evalBool " ++ show v) False = undefined
-evalBool (E (Boolean b)) = b
-evalBool e = error $ "evalBool " ++ show e
-
-equal :: Value -> Value -> Expression
-equal x y | x == y = Boolean True
-          | otherwise = Boolean False
-
-car :: Value -> Expression
-car (E (List (x:_))) = x
-car (E (List [])) = List []
-car _ = error "car applied to non list expression!"
-
-cdr :: Value -> Expression
-cdr (E (List (_:xs))) = List xs
-cdr (E (List [])) = List []
-cdr _ = error "cdr applied to non list expression!"
-
-cons :: Value -> Value -> Expression
--- cons v1 v2
---   | trace ("-> cons v1:" ++ show v1 ++ ", v2:" ++ show v2) False = undefined
-cons (E x) (E (List ys)) = List $ x:ys
-cons (E x) (E e) = List [x,e]
-
-list :: [Expression] -> Expression
-list = List
-
-nil :: Value -> Expression
-nil (E (List [])) = Boolean True
-nil _ = Boolean False
-
-not :: Value -> Expression
-not (E (Boolean False)) = Boolean True
-not _ = Boolean False
-
-or :: [Value] -> Expression
-or = Boolean . any evalBool
-
-and :: [Value] -> Expression
-and = Boolean . all evalBool
-
-pair :: Value -> Expression
-pair (E (List (x:_))) = Boolean True
-pair _ = Boolean False
-
 compareList :: (Ord a) => (a -> a -> Bool) -> [a] -> Bool
 compareList _ [] = True
 compareList f list = Prelude.and $ zipWith f list (tail list)
 
 evalExpr :: Environment -> Expression -> Value
-evalExpr env expr
-  | trace ("evalExpr " ++ show expr) False = undefined
+-- evalExpr env expr
+--   | trace ("evalExpr " ++ show expr) False = undefined
 evalExpr env (Number n)  = E $ Number n
 evalExpr env (String s)  = E $ String s
 evalExpr env (Boolean b) = E $ Boolean b
@@ -126,7 +81,6 @@ evalExpr env (Boolean b) = E $ Boolean b
 -- "quoted data is first rewritten into calls to the list construction
 -- functions before ordinary evaluation proceeds."
 -- http://www.r6rs.org/final/r6rs.pdf
-
 
 -- FIXME!!!
 evalExpr env (Quote (List lst)) = evalExpr env $ List [Symbol "list", List lst]
@@ -163,9 +117,9 @@ evalExpr env (List exprs@(x:xs)) =
         -- Primitive procedures
         "=" -> E (equal (evalExpr env (head xs))
                    (evalExpr env (head (tail xs))))
-        "or" -> E (Wiz.EvalApply.or (map (evalExpr env) xs))
-        "and" -> E (Wiz.EvalApply.and (map (evalExpr env) xs))
-        "not" -> E (Wiz.EvalApply.not (evalExpr env (head xs)))
+        "or" -> E (Wiz.Core.or (map (evalExpr env) xs))
+        "and" -> E (Wiz.Core.and (map (evalExpr env) xs))
+        "not" -> E (Wiz.Core.not (evalExpr env (head xs)))
         "null?" -> E (nil (evalExpr env (head xs)))
         "pair?" -> E (pair (evalExpr env (head xs)))
         "car" -> E (car (evalExpr env (head xs)))
@@ -196,7 +150,7 @@ evalLet env (List bindings) body = evalExpr env' body
 -- an abstraction of an expression over objects."
 
 apply :: Environment -> Value -> [Expression] -> Value
-apply env _ _ | trace ("apply in\n" ++ show env) False = undefined
+-- apply env _ _ | trace ("apply in\n" ++ show env) False = undefined
 
 apply env (C (Lambda (Formals formals) body, env')) arguments = 
   evalExpr env'' body
