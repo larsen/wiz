444c5e86ea4cb7a04853701c031ecf8f37e31a6e
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2017-08-23 22:13:06 +0200

    factored out utility code for testing

---
+++ test/Wiz/TestUtils.hs
@@ -0,0 +1,32 @@
+module Wiz.TestUtils where
+
+import           Test.Hspec
+import           Text.Parsec (parse)
+import qualified Wiz.EvalApply as W
+import           Wiz.Parser
+import           Wiz.Types
+import           Wiz.Environment
+
+import Control.Monad (foldM)
+import Data.Maybe (fromMaybe)
+import Text.ParserCombinators.Parsec.Error (
+  ParseError,
+  Message,
+  errorMessages,
+  messageEq )
+
+parseForm str = do
+  let res = parse pForm "(source)" str
+  case res of
+    Left err -> error "Something gone wrong!"
+    Right f  -> return f
+
+testForm programFiles form expectedForm = do
+  env <- foldM (\e pf ->
+                  loadProgram pf >>= (W.runProgram e . fromMaybe (Program [])))
+         emptyEnv programFiles
+  expr <- parseForm form
+  expectedExpr <- parseForm expectedForm
+  expectedResults <- W.eval expectedExpr env
+  results <- W.eval expr env
+  snd results `shouldBe` snd expectedResults
