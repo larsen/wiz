9c0654fb9e2f9ed39114e0c31c1184bb1a3ac114
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2017-08-23 17:58:47 +0200

    more generic way to express tests (it allows to pre-load multiple programs)

--- test/Wiz/EvalApplySpec.hs
+++ test/Wiz/EvalApplySpec.hs
@@ -8,6 +8,7 @@ import           Wiz.Parser
 import           Wiz.Types
 import           Wiz.Environment
 
+import Control.Monad (foldM)
 import Data.Maybe (fromMaybe)
 import Text.ParserCombinators.Parsec.Error (
   ParseError,
@@ -21,9 +22,10 @@ parseForm str = do
     Left err -> error "Something gone wrong!"
     Right f  -> return f
 
-testForm programFile form expectedForm = do
-  prg <- loadProgram programFile
-  env <- W.runProgram emptyEnv $ fromMaybe (Program []) prg
+testForm programFiles form expectedForm = do
+  env <- foldM (\e pf ->
+                  loadProgram pf >>= (W.runProgram e . fromMaybe (Program [])))
+         emptyEnv programFiles
   expr <- parseForm form
   expectedExpr <- parseForm expectedForm
   expectedResults <- W.eval expectedExpr env
@@ -64,155 +66,145 @@ spec = describe "main" $ do
 
     it "quoted list" $
       testForm
-        "init.scm"
+        ["init.scm"]
         "(car '(Harry had a heap of apples))"
         "'Harry"
 
     it "eval atom?" $
       testForm
-        "init.scm"
+        ["init.scm"]
         "(atom? 'a)"
         "#t"
 
     it "eval atom? /2" $
       testForm
-        "init.scm"
+        ["init.scm"]
         "(atom? (car '(Harry had a heap of apples)))"
         "#t"
 
     it "eval atom? /3" $
       testForm
-        "init.scm"
+        ["init.scm"]
         "(atom? '(a b c))"
         "#f"
 
     it "eval numeric expressions as integers" $
       testForm
-        "init.scm"
+        ["init.scm"]
         "1"
         "1"
 
     it "eval simple arithmetic operations" $
       testForm
-        "init.scm"
+        ["init.scm"]
         "(+ 2 2)"
         "4"
 
     it "eval simple comparison operator" $
       testForm
-        "init.scm"
+        ["init.scm"]
         "(< 1 2)"
         "#t"
 
     it "eval simple comparison operator /2" $
       testForm
-        "init.scm"
+        ["init.scm"]
         "(> 1 2)"
         "#f"
 
     it "eval simple comparison operator /3" $
       testForm
-        "init.scm"
+        ["init.scm"]
         "(> 3 2 5)"
         "#f"
 
     it "eval simple comparison operator /3" $
       testForm
-        "init.scm"
+        ["init.scm"]
         "(> 3 3 5)"
         "#f"
 
     it "eval function calls /square" $
       testForm
-        "test/square.scm"
+        ["test/square.scm"]
         "(square 10)"
         "100"
 
     it "eval recursive function calls /fact" $
       testForm
-        "init.scm"
+        ["init.scm"]
         "(fact 10)"
         "3628800"
 
     it "eval recursive function calls /map" $
       testForm
-        "init.scm"
+        ["init.scm"]
         "(map fact '(1 2 3))"
         "(1 2 6)"
 
     it "eval another recursive function calls /length" $
       testForm
-        "init.scm"
+        ["init.scm"]
         "(length '(1 2 3))"
         "3"
 
     it "eval (let) form" $
       testForm
-        "init.scm"
+        ["init.scm"]
         "(let ((a 10) (b 20)) a)"
         "10"
 
     it "eval (let) form /2" $
       testForm
-        "init.scm"
+        ["init.scm"]
         "(let ((a 10) (b 20)) (+ a b))"
         "30"
 
     it "eval closure form" $
       testForm
-        "init.scm"
+        ["init.scm"]
         "(add1 10)"
         "11"
 
     it "procedure returning closure" $
        testForm
-        "test/closure.scm"
+        ["test/closure.scm"]
         "(m 10)"
         "20"
 
-    it "count change example" $ do
-      prg <- loadProgram "init.scm"
-      prg' <- loadProgram "test/count-change.scm"
-      env <- W.runProgram emptyEnv $ fromMaybe (Program []) prg
-      env' <- W.runProgram env $ fromMaybe (Program []) prg'
-      expr <- parseForm "(count-change 100)"
-      results <- W.eval expr env'
-      results `shouldBe` (env', Just (E $ Number 292))
+    it "count change example" $
+      testForm
+        ["init.scm", "test/count-change.scm"]
+        "(count-change 100)"
+        "292"
 
   describe "set! instructions" $ do
     it "eval set!" $
       testForm
-        "test/set.scm"
+        ["test/set.scm"]
         "a"
         "20"
 
     it "eval set-car!" $
       testForm
-        "test/set-car.scm"
+        ["test/set-car.scm"]
         "list"
         "(2 2 3)"
 
-    it "eval set-cdr!" $ do
-      prg <- loadProgram "init.scm"
-      prg' <- loadProgram "test/set-cdr.scm"
-      env <- W.runProgram emptyEnv $ fromMaybe (Program []) prg
-      env' <- W.runProgram env $ fromMaybe (Program []) prg'
-      expr <- parseForm "list"
-      results <- W.eval expr env'
-      snd results `shouldBe` Just (E $ List [Number 1, Number 20, Number 30])
+    it "eval set-cdr!" $
+      testForm
+      ["init.scm", "test/set-cdr.scm"]
+      "list"
+      "(1 20 30)"
 
-    it "eval set-cdr! (it preserves list length as calculated by length)" $ do
-      prg <- loadProgram "init.scm"
-      prg' <- loadProgram "test/set-cdr.scm"
-      env <- W.runProgram emptyEnv $ fromMaybe (Program []) prg
-      env' <- W.runProgram env $ fromMaybe (Program []) prg'
-      expr <- parseForm "(length list)"
-      results <- W.eval expr env'
-      snd results `shouldBe` Just (E $ Number 3)
+    it "eval set-cdr! (it preserves list length as calculated by length)" $
+      testForm
+      ["init.scm", "test/set-cdr.scm"]
+      "(length list)"
+      "3"
 
-    it "eval set-cdr!" $ do
-      prg <- loadProgram "test/set-cdr-2.scm"
-      env <- W.runProgram emptyEnv $ fromMaybe (Program []) prg
-      expr <- parseForm "list"
-      results <- W.eval expr env
-      snd results `shouldBe` Just (E $ List [Number 1, Number 20])
+    it "eval set-cdr!" $
+      testForm
+      ["test/set-cdr-2.scm"]
+      "list"
+      "(1 20)"
